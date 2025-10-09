53d7991777efd63aa43191aa02dbcf5dc7b34d53
mdasif8393 <md.asif.8393@gmail.com>
2025-10-09 18:01:53 +0600

    57-4 Implementing Request Validation Middleware & Patient Creation with Zod Schema

--- src/app/helper/fileUploader.ts
+++ src/app/helper/fileUploader.ts
@@ -17,43 +17,4 @@ const upload = multer({ storage: storage });
 
 //Cloudinary
 
-(async function () {
-  // Configuration
-  cloudinary.config({
-    cloud_name: "dvczklwmn",
-    api_key: "823746532339489",
-    api_secret: "<your_api_secret>", // Click 'View API Keys' above to copy your API secret
-  });
-
-  // Upload an image
-  const uploadResult = await cloudinary.uploader
-    .upload(
-      "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
-      {
-        public_id: "shoes",
-      }
-    )
-    .catch((error) => {
-      console.log(error);
-    });
-
-  console.log(uploadResult);
-
-  // Optimize delivery by resizing and applying auto-format and auto-quality
-  const optimizeUrl = cloudinary.url("shoes", {
-    fetch_format: "auto",
-    quality: "auto",
-  });
-
-  console.log(optimizeUrl);
-
-  // Transform the image: auto-crop to square aspect_ratio
-  const autoCropUrl = cloudinary.url("shoes", {
-    crop: "auto",
-    gravity: "auto",
-    width: 500,
-    height: 500,
-  });
-
-  console.log(autoCropUrl);
-})();
+const uploadToCloudinary = async (file: Express.Multer.File) => {};
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+
+const createPatientValidationSchema = z.object({
+  password: z.string(),
+  patient: {
+    name: z.string({
+      error: "Name is required",
+    }),
+    email: z.string({
+      error: "Email is required",
+    }),
+    address: z
+      .string({
+        error: "Email is required",
+      })
+      .optional(),
+  },
+});
+
+export const UserValidation = {
+  createPatientValidationSchema,
+};
